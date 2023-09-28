2038625109d9a0e5f087425b14e78ca3bb3649a7
Putro Satrio Wibowo <riomabox@gmail.com>
2023-09-28 13:02:15 +0700

    Menambah asset icons, images, songs, fonts dan merubah struktur folder project

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,10 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require("tailwindcss-animated"), require("taos/plugin")],
+  safelist: [
+    "!duration-[0ms]",
+    "!delay-[0ms]",
+    'html.js :where([class*="taos:"]:not(.taos-init))',
+  ],
+  plugins: [],
 };

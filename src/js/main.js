2aacff0ec32e32d2e086f0d20997463da3bec5a7
Putro Satrio Wibowo <riomabox@gmail.com>
2023-09-29 16:58:54 +0700

    Menambahkan inisialisasi peta dengan gmaps api

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import "../css/style.css";
 import javascriptLogo from "../../assets/icons/javascript.svg";
 import viteLogo from "../../assets/icons/vite.svg";
 import { setupCounter } from "./counter.js";
+import { Loader } from "@googlemaps/js-api-loader";
 
 document.querySelector("#app").innerHTML = `
   <div>
@@ -22,3 +23,29 @@ document.querySelector("#app").innerHTML = `
 `;
 
 setupCounter(document.querySelector("#counter"));
+
+const loader = new Loader({
+  apiKey: import.meta.env.VITE_API_KEY_GMAPS,
+  version: "weekly",
+  libraries: ["places"],
+});
+
+const mapOptions = {
+  center: {
+    lat: -6.996647007867606,
+    lng: 110.41536337897409,
+  },
+  zoom: 18,
+  mapId: "4504f8b37365c3d0",
+};
+
+loader
+  .importLibrary("maps")
+  .then(async ({ Map }) => {
+    const map = new Map(document.getElementById("map"), mapOptions);
+    const { AdvancedMarkerElement } = await loader.importLibrary("marker");
+    new AdvancedMarkerElement({ map, position: mapOptions.center });
+  })
+  .catch((e) => {
+    // do something
+  });
